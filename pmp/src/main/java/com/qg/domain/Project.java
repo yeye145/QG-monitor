baa449388e74581f1e33437e5a45b10d443c8f4f
yii221 <3168707230@qq.com>
2025-08-05 17:12:45 +0800

    project实体类

--- pmp/src/main/java/com/qg/domain/Project.java
+++ pmp/src/main/java/com/qg/domain/Project.java
@@ -2,17 +2,23 @@ package com.qg.domain;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Project {
-    String uuid;
-    String name;
-    String description;
-    LocalDateTime createTime;
-    Boolean isPublic;
+    private String uuid;
+    private String name;
+    private String description;
+    private LocalDateTime createTime;
+    private Boolean isPublic;
 
     @TableLogic
     @TableField("is_deleted")
-    Boolean isDeleted;
+    private Boolean isDeleted;
 }
