40b7e96e0a75bffcf0f9e2804708a8ae29b8517e
yii221 <3168707230@qq.com>
2025-08-09 15:55:04 +0800

    feat: 加密用户信息完善

--- pmp/src/main/java/com/qg/domain/Users.java
+++ pmp/src/main/java/com/qg/domain/Users.java
@@ -2,7 +2,6 @@ package com.qg.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import jdk.jfr.DataAmount;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
