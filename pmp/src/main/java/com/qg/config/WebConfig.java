9eb22719a52b5f48b898173ef7b1811f40bdbf0a
罗烨烨 <1452756143@qq.com>
2025-08-10 10:19:33 +0800

    stash: 暂存，拉取

--- pmp/src/main/java/com/qg/config/WebConfig.java
+++ pmp/src/main/java/com/qg/config/WebConfig.java
@@ -1,22 +1,22 @@
-package com.qg.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Autowired
-    private TokenInterceptor tokenInterceptor;
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tokenInterceptor)
-                .addPathPatterns("/**") // 拦截所有路径
-                .excludePathPatterns("/users/password",
-                        "/users/code",
-                        "/users/register",
-                        "/users/sendCodeByEmail"
-                ); // 排除路径
-    }
-}
+//package com.qg.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Autowired
+//    private TokenInterceptor tokenInterceptor;
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(tokenInterceptor)
+//                .addPathPatterns("/**") // 拦截所有路径
+//                .excludePathPatterns("/users/password",
+//                        "/users/code",
+//                        "/users/register",
+//                        "/users/sendCodeByEmail"
+//                ); // 排除路径
+//    }
+//}
