c97df01f3cf271c7de60a7008736b77ee97e623c
罗烨烨 <1452756143@qq.com>
2025-08-11 20:02:41 +0800

    feat(frotend): 图-三端异常趋势走向

--- pmp/src/main/java/com/qg/mapper/FrontendErrorMapper.java
+++ pmp/src/main/java/com/qg/mapper/FrontendErrorMapper.java
@@ -2,7 +2,13 @@ package com.qg.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.qg.domain.FrontendError;
+import com.qg.vo.ErrorTrendVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @Description: 前端错误mapper  // 类说明
@@ -13,4 +19,45 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface FrontendErrorMapper extends BaseMapper<FrontendError> {
+    @Select("""
+            SELECT
+              date_trunc('hour', timestamp) AS time,
+              COUNT(*) AS value,
+              'backend' AS category
+            FROM backend_error
+            WHERE timestamp >= #{startTime}
+              AND timestamp <= #{endTime}
+              AND project_id = #{projectId}
+            GROUP BY time
+            
+            UNION ALL
+            
+            SELECT
+              date_trunc('hour', timestamp) AS time,
+              COUNT(*) AS value,
+              'frontend' AS category
+            FROM frontend_error
+            WHERE timestamp >= #{startTime}
+              AND timestamp <= #{endTime}
+              AND project_id = #{projectId}
+            GROUP BY time
+            
+            UNION ALL
+            
+            SELECT
+              date_trunc('hour', timestamp) AS time,
+              COUNT(*) AS value,
+              'mobile' AS category
+            FROM mobile_error
+            WHERE timestamp >= #{startTime}
+              AND timestamp <= #{endTime}
+               AND project_id = #{projectId}
+            GROUP BY time
+            ORDER BY time, category;
+            """)
+    List<ErrorTrendVO> getErrorTrend(
+            @Param("projectId") String projectId,
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime
+    );
 }
