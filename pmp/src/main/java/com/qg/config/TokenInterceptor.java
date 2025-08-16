7972b52602d5aebae360b71feaec6da5ae3c349b
tzdphxx <17811466341@163.com>
2025-08-16 15:09:04 +0800

    fix() : 修改token返回和拦截白名单

--- pmp/src/main/java/com/qg/config/TokenInterceptor.java
+++ pmp/src/main/java/com/qg/config/TokenInterceptor.java
@@ -1,5 +1,7 @@
 package com.qg.config;
 
+import cn.hutool.json.JSONUtil;
+import com.qg.domain.Result;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,8 +13,10 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.util.concurrent.TimeUnit;
 
+import static com.qg.domain.Code.CONFLICT;
 import static com.qg.domain.Code.UNAUTHORIZED;
 import static com.qg.utils.CryptoUtils.decryptWithAESAndRSA;
+
 @Slf4j
 @Component
 public class TokenInterceptor implements HandlerInterceptor {
@@ -24,8 +28,11 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     @Value("${rsa.key-pairs.pair2.private-key}") // 从配置读取RSA私钥
     private String rsaPrivateKey;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
         // 放行OPTIONS请求
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             return true;
@@ -43,27 +50,27 @@ public class TokenInterceptor implements HandlerInterceptor {
 
         //判断有没有密钥
         if (Rsakey == null || Rsakey.isEmpty()) {
-            response.setStatus(UNAUTHORIZED);
-            response.getWriter().write("请上传密钥");
+            Result result = new Result(UNAUTHORIZED, "没有密钥");
+            response.getWriter().write(JSONUtil.toJsonStr(result));
             return false;
         }
         if (token == null || !token.startsWith("Bearer ")) {
-            response.setStatus(UNAUTHORIZED);
-            response.getWriter().write("token不存在");
+            Result result = new Result(CONFLICT, "没有Bearer前缀");
+            response.getWriter().write(JSONUtil.toJsonStr(result));
             return false;
-        }
-        else{
+        } else {
             token = token.substring(7);
         }
 
-        log.info("token解密前"+ token);
+        log.info("token解密前" + token);
         //System.out.println(token);
-        token = decryptWithAESAndRSA(token, Rsakey,rsaPrivateKey);
+        token = decryptWithAESAndRSA(token, Rsakey, rsaPrivateKey);
 
         log.info("解密后的token：{}", token);
+        log.info("redis中查询的key：{}" + stringRedisTemplate.hasKey("login:user:" + token));
         if (!stringRedisTemplate.hasKey("login:user:" + token)) {
-            response.setStatus(UNAUTHORIZED);
-            response.getWriter().write("Token无效");
+            Result result = new Result(UNAUTHORIZED, "token无效");
+            response.getWriter().write(JSONUtil.toJsonStr(result));
             log.info("token无效");
             return false;
         }
